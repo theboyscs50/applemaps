c17205220a7cc8d286697da682078652e0861c93
wfinigan <wfinigan64@gmail.com>
2016-12-02 01:32:24 -0500

    will

---
+++ Tour Harvard/InfoView.swift
@@ -0,0 +1,13 @@
+//
+//  InfoView.swift
+//  Tour Harvard
+//
+//  Created by Will Finigan on 12/2/16.
+//  Copyright © 2016 The Boys. All rights reserved.
+//
+
+import UIKit
+
+class InfoView: UIViewController {
+
+}
